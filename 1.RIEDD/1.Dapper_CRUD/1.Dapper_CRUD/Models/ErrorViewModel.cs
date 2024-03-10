259a7f55eaa8a9a5b4b0956161730234b51072ff
Rakibul_AI <rakib.ice07@gmail.com>
2024-03-10 16:16:15 +0600

    1.Create the project.

---
+++ 1.RIEDD/1.Dapper_CRUD/1.Dapper_CRUD/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _1.Dapper_CRUD.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
