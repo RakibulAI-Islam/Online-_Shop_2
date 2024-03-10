f88273211e6b1a85f9bc6908d5ced92b2f858e75
Rakibul_AI <rakib.ice07@gmail.com>
2024-03-11 00:05:33 +0600

    4.Add Customer Controller.

---
+++ 1.RIEDD/1.Dapper_CRUD/1.Dapper_CRUD/Controllers/SqlConnection.cs
@@ -0,0 +1,11 @@
+﻿//using System.Data;
+
+//namespace _1.Dapper_CRUD.Controllers
+//{
+//    internal class SqlConnection : IDbConnection
+//    {
+//        public SqlConnection(object connectionString)
+//        {
+//        }
+//    }
+//}
