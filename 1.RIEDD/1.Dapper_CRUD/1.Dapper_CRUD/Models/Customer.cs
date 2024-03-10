fd3b18693c6d725d07e5cca50a44e0269364c1f5
Rakibul_AI <rakib.ice07@gmail.com>
2024-03-10 22:45:36 +0600

    3.Add Customer.cs class.

---
+++ 1.RIEDD/1.Dapper_CRUD/1.Dapper_CRUD/Models/Customer.cs
@@ -0,0 +1,10 @@
+﻿namespace _1.Dapper_CRUD.Models
+{
+    public class Customer
+    {
+        public int CustomerID { get; set; }
+        public string? CustomerName { get; set; }
+        public string? CustomerPhone { get; set; }
+        public string? CustomerEmail { get; set; }
+    }
+}
